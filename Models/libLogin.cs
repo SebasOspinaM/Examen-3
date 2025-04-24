b173bd875ce200109a806f3d668cc7519d03f820
juan <juanochoa1114508@correo.itm.edu.co>
2025-04-23 19:25:44 -0500

    modificado

--- Models/libLogin.cs
+++ Models/libLogin.cs
@@ -5,7 +5,19 @@ using System.Web;
 
 namespace Examen_3.Models
 {
-    public class libLogin
+    public class Login
     {
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public string PaginaSolicitud { get; set; }
+    }
+    public class LoginRespuesta
+    {
+        public string Usuario { get; set; }
+        public string Perfil { get; set; }
+        public string PaginaInicio { get; set; }
+        public bool Autenticado { get; set; }
+        public string Token { get; set; }
+        public string Mensaje { get; set; }
     }
 }
